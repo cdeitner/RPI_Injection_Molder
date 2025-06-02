e9e81aada838e8663cd14a0738801ca7d9dd8ba7
cdeitner <71476146+cdeitner@users.noreply.github.com>
2025-06-01 20:53:17 -0700

    Update max6675.py

--- max6675.py
+++ max6675.py
@@ -3,6 +3,10 @@ import time
 
 # Initialize the control pins
 def init_max6675 (sck=37, miso=35, tc_pins):
+  global SCK
+  SCK = sck
+  global MISO
+  MISO = miso
   GPIO.setmode(GPIO.BOARD)
   GPIO.setup(sck, GPIO.OUT, initial = GPIO.LOW)
   GPIO.setup(miso, GPIO.IN)
@@ -10,8 +14,32 @@ def init_max6675 (sck=37, miso=35, tc_pins):
     GPIO.setup(pin, GPIO.OUT, initial = GPIO.HIGH)
 
 def read_temp (tc):
+  data = 0
   GPIO.output(tc, GPIO.LOW)
-  time.sleep(0.00001) # is this needed?
-  GPIO.output(sck, GPIO.HIGH)
+  time.sleep(0.00001) # is this needed? 100ns min in datasheet
+  GPIO.output(SCK, GPIO.HIGH)
+  GPIO.output(SCK, GPIO.LOW)
   
+  for i in range(11, -1, -1):
+    GPIO.output(SCK, GPIO.HIGH)
+    data = data + (GPIO.input(MISO) * (2 ** i))
+    GPIO.output(SCK, GPIO.LOW)
+    
+  GPIO.output(SCK, GPIO.HIGH)
+  error = GPIO.input(MISO)
+  GPIO.output(SCK, GPIO.LOW)
+
+  for i in range(2):
+      GPIO.output(SCK, GPIO.HIGH)
+      time.sleep(0.00001) # is this needed? 100ns min in datasheet
+      GPIO.output(SCK, GPIO.LOW)  
+    
+  GPIO.output(tc, GPIO.HIGH)
+  
+  temp = data * 0.25
+  
+  if error != 0:
+      return -cs_no
+  else:
+      return temp
   
