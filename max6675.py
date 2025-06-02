0c2aaeaf283e3394a0f60673cb7cafee2169a73a
cdeitner <71476146+cdeitner@users.noreply.github.com>
2025-06-01 17:40:41 -0700

    Update max6675.py

--- max6675.py
+++ max6675.py
@@ -1,4 +1,17 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BOARD)
-GPIO.setwarnings(False)
+
+# Initialize the control pins
+def init_max6675 (sck=37, miso=35, tc_pins):
+  GPIO.setmode(GPIO.BOARD)
+  GPIO.setup(sck, GPIO.OUT, initial = GPIO.LOW)
+  GPIO.setup(miso, GPIO.IN)
+  for pin in tc_pins:
+    GPIO.setup(pin, GPIO.OUT, initial = GPIO.HIGH)
+
+def read_temp (tc):
+  GPIO.output(tc, GPIO.LOW)
+  time.sleep(0.00001) # is this needed?
+  GPIO.output(sck, GPIO.HIGH)
+  
+  
