d135e7dc037c393a3742c0c3818f238e2f0452e4
cdeitner <71476146+cdeitner@users.noreply.github.com>
2025-06-28 14:32:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,21 @@
 import max6675
+import RPi.GPIO as GPIO
+import time
+
+# Use board pin numbering
+GPIO.setmode(GPIO.BOARD)
+
+# List of CS Pins for TCs
+tcpins = [7]
+
+# SCK Pin (default 37), MISO Pin (default 35)
+max6677.init_max6675(tc_pins)
+
+while(1):  
+  temp = max6675.read_temp(7)
+  print(temp)
+  time.sleep(1)
+  
+
 
 
